ad699b5336a8ad50b0309abe5c7dce1b15832b84
Vlad Schnakovszki <shnako@yahoo.com>
2022-11-29 12:02:09 +0000

    Added mock first solution tests.

---
+++ src/test/java/com/shnako/SolutionBaseTest.java
@@ -0,0 +1,8 @@
+package com.shnako;
+
+@SuppressWarnings("unused")
+public abstract class SolutionBaseTest {
+    public abstract void testPart1();
+
+    public abstract void testPart2();
+}
---
+++ src/test/java/com/shnako/solutions/day01/SolutionTest.java
@@ -0,0 +1,21 @@
+package com.shnako.solutions.day01;
+
+import com.shnako.SolutionBaseTest;
+import com.shnako.solutions.SolutionBase;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest extends SolutionBaseTest {
+    private final SolutionBase solution = new Solution();
+
+    @Test
+    public void testPart1() {
+        assertEquals("123", solution.runPart1());
+    }
+
+    @Test
+    public void testPart2() {
+        assertEquals("456", solution.runPart2());
+    }
+}
