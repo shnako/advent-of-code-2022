b02c760dfaf64262a6f88c40afe3e423a791e44c
Vlad Schnakovszki <shnako@yahoo.com>
2022-11-29 11:52:58 +0000

    Added runner and mock first solution.

---
+++ src/main/java/com/shnako/solutions/SolutionBase.java
@@ -0,0 +1,7 @@
+package com.shnako.solutions;
+
+public abstract class SolutionBase {
+    public abstract String runPart1();
+
+    public abstract String runPart2();
+}
---
+++ src/main/java/com/shnako/solutions/day01/Solution.java
@@ -0,0 +1,15 @@
+package com.shnako.solutions.day01;
+
+import com.shnako.solutions.SolutionBase;
+
+public class Solution extends SolutionBase {
+    @Override
+    public String runPart1() {
+        return "123";
+    }
+
+    @Override
+    public String runPart2() {
+        return "456";
+    }
+}
